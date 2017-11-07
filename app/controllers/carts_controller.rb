0778b934865111e4a373bb044d41c0d0e1118149
[Ricardo Colon-Medina] <[rjcolmed@gmail.com]>
2017-11-06 23:44:04 -0500

    refactor for pithiness/readability

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -6,14 +6,12 @@ class CartsController < ApplicationController
   end
 
   def checkout
-    current_user.current_cart.line_items.each do |line_item|
+    this_cart.line_items.each do |line_item|
       item = Item.find_by(id: line_item.item_id)
-      item.inventory -= line_item.quantity
-      item.save
+      item.tap { |i| i.inventory -= line_item.quantity }.save
     end
-    cart = current_user.current_cart 
-    cart.user_id = nil
-    cart.save
-    redirect_to cart_path(cart)
+
+    this_cart.tap { |cart| cart.user_id = nil }.save
+    redirect_to cart_path(this_cart)
   end
 end
